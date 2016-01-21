c6a5cf83b03492f4be2f26baed9ea5d0c10b00fb
Jan Brecka <jan.brecka@gmail.com>
2016-01-21 22:39:07 +0100

    this is ivan

---
+++ src/cdn.js
@@ -0,0 +1,89 @@
+const Promise = require('bluebird');
+const request = require('request');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+const mime = require('./mimeType');
+
+const apiUrl = 'https://identity.api.rackspacecloud.com/v2.0/tokens';
+
+function auth(json) {
+
+  const url = json.access.serviceCatalog
+    .filter((row) => row.name === 'cloudFiles')[0].endpoints
+    .filter((row) => row.region === 'ORD')[0].publicURL;
+
+  return {
+    token: json.access.token.id,
+    url: url
+  };
+}
+
+function init(config) {
+  return new Promise((resolve, reject) => {
+    request(
+      {
+        method: 'POST',
+        url: apiUrl,
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({auth: {'RAX-KSKEY:apiKeyCredentials': {username: config.username, apiKey: config.apiKey}}})
+      },
+      (error, response, body) => {
+        if (error || response.statusCode !== 200)
+          reject(error || response.statusCode);
+        else
+          resolve(Object.assign(config, auth(JSON.parse(body))));
+      }
+    );
+  });
+}
+
+function getObjectMetadata(config, file, fileId) {
+  return new Promise((resolve, reject) => {
+    request(
+      {
+        method: 'HEAD',
+        url: config.url + '/' + config.container + '/' + fileId + path.extname(file),
+        headers: {'X-Auth-Token': config.token}
+      },
+      (error, response) => {
+        if (error)
+          reject(error);
+        else
+          resolve(response.statusCode);
+      }
+    );
+  });
+}
+
+function uploadObject(config, file, fileId) {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(file).pipe(request(
+      {
+        method: 'PUT',
+        url: config.url + '/' + config.container + '/' + fileId + path.extname(file),
+        headers: {
+          'X-Auth-Token': config.token,
+          'Content-Type': mime.getMimeTypeByExtension(path.extname(file).substr(1))
+        }
+      },
+      (error, response, body) => {
+        if (error)
+          reject(error);
+        else
+          resolve(response.statusCode);
+      }
+    ));
+  });
+}
+
+function destination(config, file, fileId) {
+  return url.resolve(config.publicUrl, '/' + fileId + path.extname(file));
+}
+
+module.exports = {
+  init,
+  getObjectMetadata,
+  uploadObject,
+  destination
+};
---
+++ src/index.js
@@ -0,0 +1,124 @@
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
+const path = require('path');
+const md5File = require('md5-file');
+const walk = require('walk');
+const cdn = require('./cdn');
+
+const encoding = 'utf8';
+const error = Symbol('ERROR');
+
+function defaultKeyMaker(key) {
+  return '{$' + key + '}';
+}
+
+function defaultValueMaker(value) {
+  return value;
+}
+
+function assign(template, assigns, keyMaker, valueMaker) {
+  keyMaker = keyMaker || defaultKeyMaker;
+  valueMaker = valueMaker || defaultValueMaker;
+
+  return assigns.reduce(
+    (template, assign) => template.replace(keyMaker(assign[0]), valueMaker(assign[1])),
+    template
+  );
+};
+
+function assignInFile(file, assigns, keyMaker, valueMaker) {
+  return fs.readFileAsync(file, encoding)
+    .then((content) => assign(content, assigns, keyMaker, valueMaker))
+    .then((content) => fs.writeFileAsync(file, content, encoding));
+}
+
+function md5FileHash(file) {
+  return new Promise((resolve, reject) => {
+    md5File(file, (error, hash) => {
+      if (error)
+        reject(error);
+      else
+        resolve(hash);
+    });
+  });
+}
+
+function artifacts(config) {
+
+  const args = Array.prototype.slice.call(arguments);
+
+  return new Promise((resolve, reject) => {
+
+    const dirs = args.slice(1);
+
+    var dirsWalked = 0;
+    var files = {};
+    var success = 0;
+    var errors = 0;
+
+    function complete() {
+      if (dirs.length === dirsWalked
+        && success + errors === Object.keys(files).length) {
+        errors === 0
+          ? resolve(files)
+          : reject(files)
+      }
+    }
+
+    cdn.init(config)
+      .then((config) => {
+        dirs.forEach((dir) => {
+          const walker = walk.walk(dir);
+          walker.on('file', (root, stats, next) => {
+            const file = path.join(root, stats.name);
+            files[file] = null;
+
+            md5FileHash(file)
+              .then((hash) => {
+                return cdn.getObjectMetadata(config, file, hash)
+                  .then((status) => {
+                    if (status === 200)
+                      return hash;
+                    if (status === 404)
+                      return cdn.uploadObject(config, file, hash)
+                        .then((status) => {
+                          if (status === 201)
+                            return hash;
+                          return error;
+                        });
+                    return error;
+                  });
+              })
+              .then((result) => {
+                if (result === error) {
+                  errors = errors + 1;
+                } else {
+                  success = success + 1;
+                  files[file] = cdn.destination(config, file, result);
+                }
+
+                complete();
+              })
+              .catch(() => {
+                errors = errors + 1;
+                complete();
+              });
+
+            next();
+          });
+          walker.on('end', () => {
+            dirsWalked++;
+            complete();
+          });
+        });
+      })
+      .catch(reject);
+  });
+
+}
+
+module.exports = {
+  assign,
+  assignInFile,
+  artifacts
+};
